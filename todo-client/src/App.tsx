802b684732fd11d193fb154fea4069e56bb8e25b
Thanasit Suwanposri <41180013+men1164@users.noreply.github.com>
2023-10-16 17:05:28 +0700

    feat: register edit todo page

--- todo-client/src/App.tsx
+++ todo-client/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import './index.css'
 import Home from './pages/Home'
+import Edit from './pages/Edit'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/edit/:id" element={<Edit />} />
       </Routes>
     </>
   )
